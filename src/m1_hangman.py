d30c455756e05dc0858a6a95aca1f0ea3c4a0638
guilfojm <guilfojm@rose-hulman.edu>
2019-05-08 15:46:19 -0400

    DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,10 +1,21 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: Justin Guilfoyle and Lauren Copeland
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
+
+import random
+
+def random_word():
+    with open('words.txt') as f:
+        f.readline()
+        string = f.read()
+        words = string.split()
+        r = random.randrange(0,len(words))
+        item = words[r]
+
 
 ####### Do NOT attempt this assignment before class! #######
 
